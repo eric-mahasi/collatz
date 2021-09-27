1544ab8ad1900e0b09a7c2383ec7c71bfa5cb9c8
Eric Mahasi <mahasieric@gmail.com>
2021-09-27 15:25:52 +0300

    Refactor: moved away from an OOP implementation as it does not provide any obvious benefits.

--- collatz.py
+++ collatz.py
@@ -7,29 +7,24 @@ and one is added. These steps are repeated until we are arrive at one."""
 # TODO add functionality to allow user to keep entering wrong numbers till they match the input criteria
 
 
-class Collatz(object):
-
-    def __init__(self):
-        self.number = 0
-        self.counter = 0
-
-    def collatz(self):
-        """Repeatedly check if number is even, number/2, if number is odd, number * 3 + 1  until number == 1"""
-        self.number = int(input("Enter a number: "))
-        while self.number != 1:
-            if self.number <= 0:
-                print("Please enter a number greater than 1.")
-                break
-            elif self.number % 2 == 0:
-                self.number /= 2
-                print(self.number)
-            else:
-                self.number = (self.number * 3) + 1
-                print(self.number)
-            self.counter += 1
+def collatz():
+    """Repeatedly check if number is even, number/2, if number is odd, number * 3 + 1  until number == 1"""
+    number = int(input("Enter a number: "))
+    counter = 0
+    while number != 1:
+        if number <= 0:
+            print("Please enter a number greater than 1.")
+            break
+        elif number % 2 == 0:
+            number /= 2
+            print(number)
+        else:
+            number = (number * 3) + 1
+            print(number)
+        counter += 1
+    print(counter, "steps taken to reach 1")
 
 
-# Objects for debugging
-a = Collatz()
-a.collatz()
-print(a.counter, "steps taken to reach 1")
+if __name__ == '__main__':
+    collatz()
+
